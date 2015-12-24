69f3c939a6244a67de1adb1098d50a1a72ce6d79
Andrew N. Shalaev <isqad88@yandex.ru>
2015-12-24 16:40:22 +0500

    gems of MC

--- spec/age_of_mc_spec.rb
+++ spec/age_of_mc_spec.rb
@@ -12,6 +12,20 @@ class MichailSogomonyan
   def gem_ninja?
     Time.now.year == 2014
   end
+
+  def gems
+    gem_list = []
+    current_date = Time.now
+
+    gem_list << 'apress-traits' if  current_date >= Time.new(2014, 9, 4)
+    gem_list << 'apress-trait_companies' if current_date >= Time.new(2014, 10, 24)
+    gem_list << 'apress-trait_rubrics' if current_date >= Time.new(2015, 2, 13)
+    gem_list << 'apress-traits-product_rubric' if current_date >= Time.new(2015, 2, 19)
+    gem_list << 'apress-traits-company_rubric' if current_date >= Time.new(2015, 2, 19)
+    gem_list << 'apress-trait_products' if current_date >= Time.new(2015, 1, 3)
+
+    gem_list
+  end
 end
 
 RSpec.describe MichailSogomonyan do
@@ -37,6 +51,47 @@ RSpec.describe MichailSogomonyan do
 
       it "became gem ninja!" do
         expect(michail).to be_gem_ninja
+      end
+
+      after { Timecop.return }
+    end
+
+    describe "his gems" do
+      context "when 2014-09-04" do
+        before { Timecop.travel(Time.new(2014, 9, 4)) }
+
+        it { expect(michail.gems).to match_array ['apress-traits'] }
+      end
+
+      context "when 2014-10-24" do
+        before { Timecop.travel(Time.new(2014, 10, 24)) }
+
+        it { expect(michail.gems).to match_array ['apress-traits', 'apress-trait_companies'] }
+      end
+
+      context "when 2015-01-03" do
+        before { Timecop.travel(Time.new(2015, 1, 3)) }
+
+        it { expect(michail.gems).to match_array ['apress-traits', 'apress-trait_companies', 'apress-trait_products'] }
+      end
+
+      context "when 2015-02-19" do
+        before { Timecop.travel(Time.new(2015, 2, 19)) }
+
+        it do
+          expect(michail.gems).to(
+            match_array(
+              [
+                'apress-traits',
+                'apress-trait_companies',
+                'apress-trait_products',
+                'apress-traits-product_rubric',
+                'apress-traits-company_rubric',
+                'apress-trait_rubrics'
+              ]
+            )
+          )
+        end
       end
 
       after { Timecop.return }
