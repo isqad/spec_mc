368358c578292e47135c664d828b30f3aa63ee74
Andrew N. Shalaev <isqad88@yandex.ru>
2015-12-24 17:17:27 +0500

    fixes

--- spec/age_of_mc_spec.rb
+++ spec/age_of_mc_spec.rb
@@ -69,116 +69,116 @@ RSpec.describe MichailSogomonyan do
 
   describe "A long time ago in a galaxy far, far away" do
     context "When 05/09/2013" do
-      it "starts work in blizko" do
+      it "starts work at blizko" do
         expect(michail.start_date).to eq '05/09/2013'
       end
     end
-  end
 
-  describe "first projects" do
-    it "are \"knife\" and \"knocker\"" do
-      expect(michail.first_projects).to match_array ['knife', 'knocker']
-    end
-  end
-
-  describe "as blizko developer" do
-    context "when was 2014" do
-      before { Timecop.travel(Time.new(2014, 1, 1)) }
-
-      it "became gem ninja!" do
-        expect(michail).to be_gem_ninja
+    describe "first projects" do
+      it "are \"knife\" and \"knocker\"" do
+        expect(michail.first_projects).to match_array ['knife', 'knocker']
       end
-
-      after { Timecop.return }
     end
 
-    describe "his gems" do
-      context "when 2014-09-04" do
-        before { Timecop.travel(Time.new(2014, 9, 4)) }
+    describe "as blizko developer" do
+      context "when was 2014" do
+        before { Timecop.travel(Time.new(2014, 1, 1)) }
 
-        it { expect(michail.gems).to match_array ['apress-traits'] }
-      end
-
-      context "when 2014-10-24" do
-        before { Timecop.travel(Time.new(2014, 10, 24)) }
-
-        it { expect(michail.gems).to match_array ['apress-traits', 'apress-trait_companies'] }
-      end
-
-      context "when 2015-01-03" do
-        before { Timecop.travel(Time.new(2015, 1, 3)) }
-
-        it { expect(michail.gems).to match_array ['apress-traits', 'apress-trait_companies', 'apress-trait_products'] }
-      end
-
-      context "when 2015-02-19" do
-        before { Timecop.travel(Time.new(2015, 2, 19)) }
-
-        it do
-          expect(michail.gems).to(
-            match_array(
-              [
-                'apress-traits',
-                'apress-trait_companies',
-                'apress-trait_products',
-                'apress-traits-product_rubric',
-                'apress-traits-company_rubric',
-                'apress-trait_rubrics'
-              ]
-            )
-          )
+        it "became gem ninja!" do
+          expect(michail).to be_gem_ninja
         end
-      end
-
-      after { Timecop.return }
-    end
-
-    describe "stay tunde..." do
-      context "from 30/03/2015" do
-        before { Timecop.travel(Time.new(2015, 3, 30)) }
-
-        it { expect(michail).to be_teamleader }
 
         after { Timecop.return }
       end
 
-      context "when 12/11/2015" do
-        it "was his first rollback" do
-          expect(michail.first_rollback_at.to_i).to eq Time.new(2015, 11, 12).to_i
+      describe "his gems" do
+        context "when 2014-09-04" do
+          before { Timecop.travel(Time.new(2014, 9, 4)) }
+
+          it { expect(michail.gems).to match_array ['apress-traits'] }
+        end
+
+        context "when 2014-10-24" do
+          before { Timecop.travel(Time.new(2014, 10, 24)) }
+
+          it { expect(michail.gems).to match_array ['apress-traits', 'apress-trait_companies'] }
+        end
+
+        context "when 2015-01-03" do
+          before { Timecop.travel(Time.new(2015, 1, 3)) }
+
+          it { expect(michail.gems).to match_array ['apress-traits', 'apress-trait_companies', 'apress-trait_products'] }
+        end
+
+        context "when 2015-02-19" do
+          before { Timecop.travel(Time.new(2015, 2, 19)) }
+
+          it do
+            expect(michail.gems).to(
+              match_array(
+                [
+                  'apress-traits',
+                  'apress-trait_companies',
+                  'apress-trait_products',
+                  'apress-traits-product_rubric',
+                  'apress-traits-company_rubric',
+                  'apress-trait_rubrics'
+                ]
+              )
+            )
+          end
+        end
+
+        after { Timecop.return }
+      end
+
+      describe "stay tuned..." do
+        context "from 30/03/2015" do
+          before { Timecop.travel(Time.new(2015, 3, 30)) }
+
+          it { expect(michail).to be_teamleader }
+
+          after { Timecop.return }
+        end
+
+        context "when 12/11/2015" do
+          it "was his first rollback" do
+            expect(michail.first_rollback_at.to_i).to eq Time.new(2015, 11, 12).to_i
+          end
+        end
+
+        it "releases barmen project" do
+          expect(michail).to be_barmen
+        end
+
+        it "releases customer cart to db" do
+          expect(michail.customer_cart_to_db).to eq true
+        end
+
+        it "releases duty bachelor" do
+          expect(michail.duty_bachelor).to eq true
+        end
+
+        it "completes so much awesome code" do
+          expect(michail.a_lot_awesome_code).to eq [true, true]
         end
       end
-
-      it "releases barmen project" do
-        expect(michail).to be_barmen
-      end
-
-      it "releases customer cart to db" do
-        expect(michail.customer_cart_to_db).to eq true
-      end
-
-      it "releases duty bachelor" do
-        expect(michail.duty_bachelor).to eq true
-      end
-
-      it "completes so much awesome code" do
-        expect(michail.a_lot_awesome_code).to eq [true, true]
-      end
-    end
-  end
-
-  describe "statistics" do
-    it "300 tasks count" do
-      expect(michail.tasks_count).to eq 300
     end
 
-    it "2500 merged PR's" do
-      expect(michail.merged_pull_requests_count).to eq 2500
-    end
-  end
+    describe "statistics" do
+      it "300 tasks count" do
+        expect(michail.tasks_count).to eq 300
+      end
 
-  describe "Thank you!" do
-    it "best friend" do
-      expect(michail).to be_best
+      it "2500 merged PR's" do
+        expect(michail.merged_pull_requests_count).to eq 2500
+      end
+    end
+
+    describe "Thank you!" do
+      it "best friend" do
+        expect(michail).to be_best
+      end
     end
   end
 end
