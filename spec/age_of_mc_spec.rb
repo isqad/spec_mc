e5987aea72168c97d42dd5a78bf780e6bb7cabc9
Andrew N. Shalaev <isqad88@yandex.ru>
2015-12-24 15:30:52 +0500

    feat: tested Michail class

--- spec/age_of_mc_spec.rb
+++ spec/age_of_mc_spec.rb
@@ -1,11 +1,31 @@
 require 'rspec'
 
-RSpec.describe "Michail Sogomonyan" do
+class MichailSogomonyan
+  attr_reader :start_date
+
+  def initialize(start_date)
+    @start_date = start_date
+  end
+
+  def first_projects
+    %w(knife knocker)
+  end
+end
+
+RSpec.describe MichailSogomonyan do
+  let(:michail) { described_class.new('05/09/2013') }
+
   describe "A long time ago in a galaxy far, far away" do
     context "When 05/09/2013" do
       it "starts work in blizko" do
-        expect(true).to eq true
+        expect(michail.start_date).to eq '05/09/2013'
       end
+    end
+  end
+
+  describe "first projects" do
+    it "are \"knife\" and \"knocker\"" do
+      expect(michail.first_projects).to match_array ['knife', 'knocker']
     end
   end
 end
