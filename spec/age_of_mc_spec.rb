48a2405ea1fed996b2d9f9533bda64e83d6bc0be
Andrew N. Shalaev <isqad88@yandex.ru>
2015-12-24 15:18:38 +0500

    Initial commit

---
+++ spec/age_of_mc_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+
+RSpec.describe "Michail Sogomonyan" do
+  describe "A long time ago in a galaxy far, far away" do
+    context "When 05/09/2013" do
+      it "starts work in blizko" do
+        expect(true).to eq true
+      end
+    end
+  end
+end
